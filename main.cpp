74817bc026385f60ee0d741d9f2b209274635f86
John Varga <john.varga@unifilabs.com>
2018-03-02 22:46:41 -0800

    hold space to zoom. only render after zooming a step

--- main.cpp
+++ main.cpp
@@ -56,63 +56,67 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        // zoom
-        max -= 0.1 * factor;
-        min += 0.15 * factor;
-        factor *= 0.9349;
-        ITERATIONS += 5;
-
-        if (count > 30) {
-            // bump iterations for better accuracy as we zoom in
+        const Uint8 *state = SDL_GetKeyboardState(NULL);
+        if (state[SDL_SCANCODE_Q]) {
+            quit = true;
+            break;
+        }
+        if (state[SDL_SCANCODE_ESCAPE] || state[SDL_SCANCODE_DELETE]) {
+            reset();
+            count = 0;
+        }
+        if (state[SDL_SCANCODE_SPACE]) {
+            // zoom
+            max -= 0.1 * factor;
+            min += 0.15 * factor;
+            factor *= 0.9349;
             ITERATIONS += 5;
-        }
-
-        SDL_SetRenderDrawColor(renderer, 0x37, 0x2f, 0xbe, 0xff);
-        SDL_RenderClear(renderer);
-
-        LAST = NOW;
-        NOW = SDL_GetPerformanceCounter();
-        deltaTime = (double)((NOW - LAST) * 1000 / (double)SDL_GetPerformanceFrequency());
-
-        for (int x = 0; x < W; x++) {
-            for (int y = 0; y < H; y++) {
-
-                float cr = map(x, 0, W, min, max);
-                float ci = map(y, 0, H, min, max);
-
-                float zr = 0.0;
-                float zi = 0.0;
-                float zrsq = zr * zr;
-                float zisq = zi * zi;
-
-                int n = 0;
-                while (n < ITERATIONS && zrsq + zisq <= 4.0) {
-                    zi = (zr + zi) * (zr + zi) - zrsq - zisq;
-                    zi += ci;
-                    zr = zrsq - zisq + cr;
-
-                    zrsq = zr * zr;
-                    zisq = zi * zi;
-                    n++;
-                }
-
-                int bright = map(n, 0, ITERATIONS, 0, 255);
-
-                if (n == ITERATIONS || bright < 20) {
-                    bright = 0;
-                }
-
-                int red = map(bright * bright, 0, 6502, 0, 255);
-                int green = bright;
-                int blue = map(sqrt(bright), 0, sqrt(255), 0, 255);
-
-                SDL_SetRenderDrawColor(renderer, red, green, blue, 255);
-                SDL_RenderDrawPoint(renderer, x, y);
+            if (count > 30) {
+                // bump iterations for better accuracy as we zoom in
+                ITERATIONS += 5;
             }
+            SDL_SetRenderDrawColor(renderer, 0x37, 0x2f, 0xbe, 0xff);
+            SDL_RenderClear(renderer);
+
+            for (int x = 0; x < W; x++) {
+                for (int y = 0; y < H; y++) {
+
+                    float cr = map(x, 0, W, min, max);
+                    float ci = map(y, 0, H, min, max);
+
+                    float zr = 0.0;
+                    float zi = 0.0;
+                    float zrsq = zr * zr;
+                    float zisq = zi * zi;
+
+                    int n = 0;
+                    while (n < ITERATIONS && zrsq + zisq <= 4.0) {
+                        zi = (zr + zi) * (zr + zi) - zrsq - zisq;
+                        zi += ci;
+                        zr = zrsq - zisq + cr;
+
+                        zrsq = zr * zr;
+                        zisq = zi * zi;
+                        n++;
+                    }
+
+                    int bright = map(n, 0, ITERATIONS, 0, 255);
+
+                    if (n == ITERATIONS || bright < 20) {
+                        bright = 0;
+                    }
+
+                    int red = map(bright * bright, 0, 6502, 0, 255);
+                    int green = bright;
+                    int blue = map(sqrt(bright), 0, sqrt(255), 0, 255);
+
+                    SDL_SetRenderDrawColor(renderer, red, green, blue, 255);
+                    SDL_RenderDrawPoint(renderer, x, y);
+                }
+            }
+
+            SDL_RenderPresent(renderer);
         }
-
-        SDL_RenderPresent(renderer);
-
     }
 
     SDL_DestroyRenderer(renderer);
